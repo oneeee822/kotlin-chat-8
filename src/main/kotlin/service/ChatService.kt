eed9396590b8ed55dcf032633983de0dcc3fedd3
OneE <bestoneeee@naver.com>
2025-11-24 00:57:05 +0900

    feat: 사용자 입장 및 퇴장 로직 구현

---
+++ src/main/kotlin/service/ChatService.kt
@@ -0,0 +1,27 @@
+package service
+
+import io.ktor.websocket.*
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.collections.LinkedHashSet
+
+class ChatService {
+
+    private val userCounter = AtomicInteger(0)
+
+    // 접속자 관리
+    private val connections = Collections.synchronizedSet(LinkedHashSet<DefaultWebSocketSession>())
+
+    // 유저 입장
+    fun joinUser(session: DefaultWebSocketSession): String {
+        connections += session
+        val id = userCounter.incrementAndGet()
+        val prefix = listOf("픽셀", "레트로", "8비트", "용사", "마법사").random()
+        return "${prefix}플레이어$id"
+    }
+
+    // 유저 퇴장
+    fun leaveUser(session: DefaultWebSocketSession) {
+        connections -= session
+    }
+}
